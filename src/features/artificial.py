6e0a9afb058a9fec1cc71a24df821aa3a56875cc
mikolajkania <mikolajkania@gmail.com>
2022-07-08 00:11:54 +0200

    Binarizing is not working

--- src/features/artificial.py
+++ src/features/artificial.py
@@ -8,11 +8,6 @@ class FeatureCreator:
         FeatureCreator._extend_porch(data)
 
     @staticmethod
-    def _create_binary(data: pd.DataFrame, feature: str):
-        data[feature + 'binary'] = data[feature].apply(lambda x: 1 if x > 0 else 0)
-        data[feature + 'binary'].astype(bool)
-
-    @staticmethod
     def _extend_porch(data: pd.DataFrame):
         data['PorchArea_artificial'] = (data['OpenPorchSF'] + data['EnclosedPorch'] +
                                         data['3SsnPorch'] + data['ScreenPorch'])
