4be4459fdc5450235177989ab1443bc230277649
mikolajkania <mikolajkania@gmail.com>
2022-07-08 00:19:09 +0200

    Adding TotalUtilityArea feature

--- src/features/artificial.py
+++ src/features/artificial.py
@@ -7,6 +7,8 @@ class FeatureCreator:
     def create_new(data: pd.DataFrame):
         FeatureCreator._extend_porch(data)
 
+        data['TotalUtilityArea_artificial'] = (data['GrLivArea'] + data['TotalBsmtSF'] + data['GarageArea'])
+
     @staticmethod
     def _extend_porch(data: pd.DataFrame):
         data['PorchArea_artificial'] = (data['OpenPorchSF'] + data['EnclosedPorch'] +
